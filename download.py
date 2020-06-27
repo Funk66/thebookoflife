35f326ff150b3fb85120ed77ede92f56f9f4e40b
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2020-06-27 08:37:55 +0200

    Refactor and cleanup

--- download.py
+++ download.py
@@ -1,111 +1,194 @@
+from re import sub, match
+from time import sleep
+from shutil import copyfileobj
+from typing import List
 from pathlib import Path
-from urllib.request import Request, urlopen, urlretrieve
+from requests import get
+from urllib.request import urljoin
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 from bs4.element import Tag
+from alive_progress import alive_bar
+from typing import overload, Optional
 
 
-def get(url):
-    request = Request(
-        url,
-        headers={
-            "User-Agent": (
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                "Chrome/35.0.1916.47 Safari/537.36"
-            )
-        },
-    )
-    html = urlopen(request)
-    return BeautifulSoup(html, "html.parser")
+class Item:
+    title: str
+    name: str
+
+    def __str__(self) -> str:
+        return self.title
+
+    def __repr__(self) -> str:
+        return self.name
+
+    @staticmethod
+    def sanitize(text: str) -> str:
+        return sub(r"\W", "", text)
 
 
-INDEX = """.. _{section_link}:
+class Book(Item):
+    title = "The Book of Life"
+    url = "https://theschooloflife.com/thebookoflife/"
+    path = Path(__file__).parent / "source"
+    parts: List["Part"] = []
 
-{stars}
-{section_title}
-{stars}
+    @classmethod
+    def download(cls) -> None:
+        with alive_bar() as bar:
+            for html in fetch(cls.url)(class_="nav-main__sub-rollover"):
+                if html.has_attr("onmouseover"):
+                    cls.parts.append(Part(html.text))
+            for part in cls.parts:
+                bar(part.title)
+                part.download()
+        chapters = [
+            chapter
+            for part in cls.parts
+            for section in part.sections
+            for chapter in section.chapters
+        ]
+        with alive_bar(len(chapters)) as bar:
+            for part in cls.parts:
+                for section in part.sections:
+                    for chapter in section.chapters:
+                        if not (chapter.path / "text.rst").exists():
+                            chapter.download()
+                            chapter.write()
+                            sleep(10)
+                        bar(chapter.title)
+                    section.write()
+                part.write()
 
-.. toctree::
 
-{articles}
-"""
-IMAGE = """
-.. figure:: {path}
-   :figwidth: 100 %
+class Part(Item):
+    sections: List["Section"] = []
 
-   {caption}
-"""
-base_url = "https://theschooloflife.com/thebookoflife"
-page = get(base_url)
-categories = [
-    cat.text
-    for cat in page(class_="nav-main__sub-rollover")
-    if cat.has_attr("onmouseover")
-]
-for category in categories:
-    category_path = category.lower().replace(" ", "_")
-    print(f"{categories.index(category)+1}/{len(categories)} {category}")
-    page = get(f"{base_url}/category/{category}/?index")
-    for section in (sections := page("section")) :
-        section_title = section.div.text
-        section_link = section_title.lower().replace(" ", "_")
-        print(f" {sections.index(section)+1}/{len(sections)} {section_title}")
-        image = 0
-        filenames = []
-        for article in (articles := section("li")) :
-            title = article.find(class_="title").text
-            filename = sub("\W", "", title.replace(" ", "_").lower())
-            filenames += [filename]
-            print(f"  {articles.index(article)+1}/{len(articles)} {title}")
-            page = get(article.a.attrs["href"])
-            text = [f"{title}\n{'-'*len(title)}"]
-            path = Path(f"source/{category_path}/{section_link}/")
-            path.mkdir(parents=True, exist_ok=True)
-            for paragraph in page.find(class_="old-wrapper").children:
-                if not isinstance(paragraph, Tag):
-                    continue
-                elif paragraph.figure:
-                    image += 1
-                    imagepath = path / f"{image}.jpg"
-                    urlretrieve(paragraph.img.attrs["src"], imagepath)
-                    caption = True
-                    continue
-                elif paragraph.em and caption and len(paragraph.text) < 100:
-                    text += [
-                        IMAGE.format(
-                            path=f"{image}.jpg", caption=paragraph.text.strip()
-                        )
-                    ]
-                elif paragraph.p or paragraph.name == "p":
-                    if caption:
-                        text += [
-                            IMAGE.format(
-                                path=f"{image}.jpg", caption=paragraph.text.strip()
-                            )
-                        ]
-                    text += [paragraph.text.strip(), "\n\n"]
-                caption = False
-            with open(path / f"{filename}.rst", "w") as output:
-                output.write("".join(text))
-            break
-        with open(path / "index.rst", "w") as output:
-            output.write(
-                INDEX.format(
-                    section_link=section_link,
-                    section_title=section_title,
-                    stars="*" * len(section_title),
-                    articles="\n".join([f"   {fn}" for fn in filenames]),
-                )
-            )
-        break
-    break
-title = "The Book of Life"
-with open(path.parent / "index.rst", "w") as output:
-    output.write(
-        INDEX.format(
-            section_link="",
-            section_title=title,
-            stars="*" * len(title),
-            articles="\n".join([f"   {cat}" for cat in categories]),
+    def __init__(self, title: str):
+        self.title = title
+        self.name = self.sanitize(title)
+        self.url = urljoin(Book.url, f"category/{title.lower()}/?index")
+        self.path = Book.path / self.name
+
+    def download(self) -> None:
+        html = fetch(self.url)
+        self.sections = [Section(section, self) for section in html.find_all("section")]
+        for section in self.sections:
+            continue
+            section.download()
+
+    def write(self) -> None:
+        index = (
+            f".. _{self.name}\n\n"
+            f"{self.title}\n{'='*len(self.title)}\n\n"
+            ".. toctree::\n"
         )
-    )
+        for section in self.sections:
+            index += f"   {section.name}/index\n"
+        with open(self.path / "index.rst", "w") as output:
+            output.write(index)
+
+
+class Section(Item):
+    chapters: List["Chapter"] = []
+
+    def __init__(self, html: Tag, part: Part):
+        self.title = html.div.text
+        self.part = part
+        self.name = self.sanitize(self.title)
+        self.path = part.path / self.name
+        self.chapters = [Chapter(chapter, self) for chapter in html.find_all("li")]
+
+    def write(self) -> None:
+        index = f".. _{self.name}\n\n"
+        index += f"{self.title}\n{'='*len(self.title)}\n\n.. toctree::\n"
+        for chapter in self.chapters:
+            index += f"   {chapter.name}/text\n"
+        with open(self.path / "index.rst", "w") as output:
+            output.write(index)
+
+
+class Chapter(Item):
+    headings: List[str] = [r"^\n+\.", r"^\(?[iIvVxX]+\)?\.?"]
+
+    def __init__(self, html: Tag, section: Section):
+        self.title = html(class_="title")[0].text
+        self.name = self.sanitize(self.title)
+        self.section = section
+        self.url = html.a.attrs["href"]
+        self.path = section.path / self.name
+
+    def download(self) -> None:
+        try:
+            self.html = fetch(self.url)
+        except HTTPError as error:
+            message = f"{self.url}: {error.code} - {error.reason}"
+            raise HTTPError(self.url, error.code, message, None, None)
+
+    def write(self) -> None:
+        images = 0
+        caption = False
+        text = f"{self.title}\n{'='*len(self.title)}\n\n"
+        self.path.mkdir(parents=True, exist_ok=True)
+        for html in self.html.find(class_="old-wrapper").children:
+            if not isinstance(html, Tag):
+                continue
+            elif {"responsive-container", "addtoany_content"} & set(
+                html.attrs.get("class", [])
+            ):
+                break
+            elif html.img:
+                images += 1
+                path = self.path / f"{images}.jpg"
+                if not path.exists():
+                    try:
+                        fetch(html.img.attrs["src"], path)
+                    except Exception:
+                        print(f"Failed to fetch image {html.img.attrs.get('src')}")
+                        continue
+                text += f"\n.. figure:: {images}.jpg\n   :figwidth: 100 %\n\n"
+                caption = True
+            elif html.p or html.em or html.name in ["em", "p"]:
+                for heading in self.headings:
+                    if match(heading, html.text):
+                        text += html.text + "\n" + "-" * len(html.text)
+                        caption = False
+                        continue
+                if caption and len(html.text) < 100:
+                    text += "   "
+                text += html.text.strip() + "\n\n"
+                caption = False
+        with open(self.path / "text.rst", "w") as output:
+            output.write(text)
+
+
+@overload
+def fetch(url: str) -> BeautifulSoup:
+    ...
+
+
+@overload
+def fetch(url: str, path: Path = None) -> None:
+    ...
+
+
+def fetch(url: str, path: Path = None) -> Optional[BeautifulSoup]:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/35.0.1916.47 Safari/537.36"
+    }
+    if path:
+        response = get(url, headers=headers, stream=True)
+        with open(path, "wb") as output:
+            copyfileobj(response.raw, output)
+        return None
+    else:
+        html = get(url, headers=headers)
+        if html.status_code == 503:
+            raise HTTPError(url, 503, "Rate limited", None, None)
+        return BeautifulSoup(html.text, "html.parser")
+
+
+if __name__ == "__main__":
+    Book.download()
